b51a13cae35c8efd80fb53a776c3e38992597053
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-02-02 20:25:07 +0430

    feat: Add router and Navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import Product from './pages/Product';
+import PlaceOrder from './pages/PlaceOrder';
+import Collection from './pages/Collection';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
   )
 }
 
